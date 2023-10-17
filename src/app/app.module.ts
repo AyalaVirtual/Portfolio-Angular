090182086c9f2c190cbc31cf3158d1fd51c0b857
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-17 16:27:23 -0400

    Add exisiting project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    PortfolioComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
